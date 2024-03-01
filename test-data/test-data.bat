7c659db7fbd9f9545c8e329191807bb530958162
GameKnave <git@gameknave.com>
2024-03-01 09:15:39 -0800

    [tests] client test watch + project batch cleanup

--- test-data/test-data.bat
+++ test-data/test-data.bat
@@ -1,2 +1,2 @@
-cd "server" && "npx jasmine" && ^
-cd "../client" && ng "test" "--source-map=false" "--browsers=ChromeHeadless" "--watch=false"
+cd "server" && "npm test" && ^
+cd "../client" && "npm test"
