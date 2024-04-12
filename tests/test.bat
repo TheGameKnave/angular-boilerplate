73defbde52ae08bfb278674c107995f6f0bbf96c
Ralph L Robinson <ralphrobinson@student.olympic.edu>
2024-04-12 09:53:27 -0700

    [update] Added TestCafe-Blink-Diff to test.bat
    Updated test.bat with readme shell commands for Testcafe-Blink-Diff, having it run after standard TestCafe tests.

--- tests/test.bat
+++ tests/test.bat
@@ -2,4 +2,6 @@
 
 cd server && npm test && ^
 cd ../client && npm test && ^
-cd ../ && npx testcafe chrome tests --app "npm run dev"
+cd ../ && npx testcafe chrome tests --app "npm run dev" && ^
+npx testcafe chrome tests -s tests/e2e/screenshots --take-snapshot actual --app "npm run dev" && ^
+npx testcafe-blink-diff tests/e2e/screenshots --compare base:actual --open --threshold 0.005
