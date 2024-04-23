8c9ef393d36b8722b190af5e50b889e577886a55
Ralph L Robinson <ralphrobinson@student.olympic.edu>
2024-04-23 09:22:37 -0700

    Testing page load time

--- tests/e2e/test.ts
+++ tests/e2e/test.ts
@@ -1,12 +1,25 @@
 import { AngularSelector, waitForAngular } from 'testcafe-angular-selectors';
 import { takeSnapshot } from 'testcafe-blink-diff';
+import { Selector } from 'testcafe';
 
 fixture `App tests`
     .page('http://localhost:4200')
     .beforeEach(async () => {
         await waitForAngular();
     });
-    
+test('Measure Page Load Time', async t => {
+    const startTime = await t.eval(() => performance.now());
+
+    await t
+        .wait(1000) // Wait for the page to fully load
+        .expect(Selector('body').exists).ok() // Wait for the body element to be present
+        .wait(500); // Wait for the page to render
+
+    const endTime = await t.eval(() => performance.now());
+    const pageLoadTime = endTime - startTime;
+
+    console.log(`Page load time: ${pageLoadTime} milliseconds`);
+});
 test('Click Button Once', async t => {
     await t
         // .takeScreenshot('App_tests/Click_Button_Test/test-screenshot-1.png')
