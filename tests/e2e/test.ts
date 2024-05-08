ea470175ed94b00b98a15f4dadf6d16eecde3dc5
Ralph L Robinson <ralphrobinson@student.olympic.edu>
2024-05-08 09:42:51 -0700

    [test suite] added page load time test
    Added test that measures page load time.
    Flags if over 25 milliseconds.
    Gonna make a scaling function for that threshold later.

--- tests/e2e/test.ts
+++ tests/e2e/test.ts
@@ -1,23 +1,26 @@
 import { AngularSelector, waitForAngular } from 'testcafe-angular-selectors';
 import { takeSnapshot } from 'testcafe-blink-diff';
 import { Selector } from 'testcafe';
+import { ClientFunction } from 'testcafe';
+
+// page load time stuff
+const isPageLoaded = ClientFunction(() => {return document.readyState === 'complete';});
+const pageLoadTimeThreshold = 25; // Threshold in milliseconds. Decided by function later
 
 fixture `App tests`
     .page('http://localhost:4200')
     .beforeEach(async () => {
         await waitForAngular();
     });
+// measures page load time, fails if load time over threshold
 test('Measure Page Load Time', async t => {
     const startTime = await t.eval(() => performance.now());
 
-    await t
-        .wait(1000) // Wait for the page to fully load
-        .expect(Selector('body').exists).ok() // Wait for the body element to be present
-        .wait(500); // Wait for the page to render
+    await t.expect(isPageLoaded()).ok();
 
     const endTime = await t.eval(() => performance.now());
     const pageLoadTime = endTime - startTime;
-
+    await t.expect(pageLoadTime).lt(pageLoadTimeThreshold);
     console.log(`Page load time: ${pageLoadTime} milliseconds`);
 });
 test('Click Button Once', async t => {
