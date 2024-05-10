6fcc9dfc5a50adb2013d9a3732272366db054815
Ralph L Robinson <ralphrobinson@student.olympic.edu>
2024-05-10 10:38:49 -0700

    [testing] added memory usage test

--- tests/e2e/test.ts
+++ tests/e2e/test.ts
@@ -3,9 +3,33 @@ import { takeSnapshot } from 'testcafe-blink-diff';
 import { Selector } from 'testcafe';
 import { ClientFunction } from 'testcafe';
 
+// making memory exception. Might want to move to global ts file
+declare global {
+    interface Performance {
+        memory: {
+            jsHeapSizeLimit: number;
+            totalJSHeapSize: number;
+            usedJSHeapSize: number;
+        };
+    }
+}
 // page load time stuff
 const isPageLoaded = ClientFunction(() => {return document.readyState === 'complete';});
 const pageLoadTimeThreshold = 25; // Threshold in milliseconds. Decided by function later
+
+// helper functions for Measure Memory
+const getMemory = async t => JSON.parse(
+    await t.eval(() => {
+    const { jsHeapSizeLimit, totalJSHeapSize, usedJSHeapSize } = window.performance.memory;
+    console.log({ jsHeapSizeLimit, totalJSHeapSize, usedJSHeapSize });
+    return JSON.stringify({ jsHeapSizeLimit, totalJSHeapSize, usedJSHeapSize });
+}));
+const validateMemory = (memoryVal: { jsHeapSizeLimit: number; usedJSHeapSize: number; totalJSHeapSize: number }, threshold: number) => {
+    const { jsHeapSizeLimit, usedJSHeapSize, totalJSHeapSize } = memoryVal;
+    console.log(memoryVal.usedJSHeapSize);
+    const isMemoryLowerThanThreshold =  usedJSHeapSize < threshold;
+    return isMemoryLowerThanThreshold;
+};
 
 fixture `App tests`
     .page('http://localhost:4200')
@@ -25,17 +49,19 @@ test('Measure Page Load Time', async t => {
 });
 test('Click Button Once', async t => {
     await t
-        // .takeScreenshot('App_tests/Click_Button_Test/test-screenshot-1.png')
         .click('button');
-        // .takeScreenshot('App_tests/Click_Button_Test/test-screenshot-2.png');
 
         await takeSnapshot(t);
 });
 test('Click Button Twice', async t => {
     await t
-        // .takeScreenshot('App_tests/Click_Button_Test_2/test-screenshot-1.png')
         .click('button')
         .click('button');
-        // .takeScreenshot('App_tests/Click_Button_Test_2/test-screenshot-2.png');
     await takeSnapshot(t);
-})
+});
+test('Measure Memory Usage', async t => {
+    console.log(await getMemory(t));
+    const memoryVal = await getMemory(t);
+    const threshold = 80000000;
+    await t.expect(validateMemory(memoryVal, threshold)).ok(`Memory usage exceeds the threshold: ${JSON.stringify(memoryVal)}`);
+});
