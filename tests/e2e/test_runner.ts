b59108bb9202c30dd9d3b2e6a5c2c3d1aca98ef3
Ralph L Robinson <ralphrobinson@student.olympic.edu>
2024-05-22 10:11:12 -0700

    [Testcafe] tinkering with test-runner. Added startApp to run dev immediately in the test runner

--- tests/e2e/test_runner.ts
+++ tests/e2e/test_runner.ts
@@ -7,8 +7,9 @@ const createTestCafe = require("testcafe");
     const runner = testcafe.createRunner();
 
     const failedCount = await runner
-      .src(["./test_homepage/test.ts"])
+      .startApp("npm run dev")
       .browsers(["chrome --window-size=1280,1024"])
+      .src(["tests/e2e/test_homepage/test.ts"])
       .run();
 
     console.log("Tests failed: " + failedCount);
