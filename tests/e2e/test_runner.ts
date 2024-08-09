97a2022a7cf02f0a02765fbe0d68ac1bc6dd2fa1
GameKnave <git@gameknave.com>
2024-08-09 09:36:38 -0700

    testingtesttesting - test runner

--- tests/e2e/test_runner.ts
+++ tests/e2e/test_runner.ts
@@ -1,18 +1,39 @@
 const createTestCafe = require("testcafe");
+const { exec } = require("child_process");
 
 (async () => {
-  const testcafe = await createTestCafe("localhost", 4200);
+  const serverProcess = await exec("npm run prod");
+  const testcafe = await createTestCafe();
 
   try {
     const runner = testcafe.createRunner();
 
     const failedCount = await runner
-      .browsers(["chrome --window-size=1280,1024"])
-      .src(["tests/e2e/run/test.ts"])
-      .run();
+      .browsers([
+        "chrome:headless --window-size=1280,1024",
+        "firefox:headless --window-size=1280,1024",
+        "safari --window-size=1280,1024",
+        "edge --window-size=1280,1024",
+        "opera --window-size=1280,1024",
+        "chrome:emulation:device=iphone X"
+      ])
+      .src(["run/test.ts"]) // Specify the test files or directories
+      .screenshots({
+        path: "screenshots", // Set the screenshot path
+        takeOnFails: true // Take screenshots on failures
+      })
+      .reporter("spec") // Optional: Choose a reporter, e.g., spec, list, etc.
+      .run({
+        skipJsErrors: true, // Handle JS errors gracefully
+        selectorTimeout: 5000, // Increase selector timeout if needed
+        assertionTimeout: 7000, // Increase assertion timeout if needed
+        pageLoadTimeout: 8000, // Increase page load timeout if needed
+        speed: 1, // Adjust the test speed (1 is the default)
+      });
 
     console.log("Tests failed: " + failedCount);
   } finally {
     await testcafe.close();
+    serverProcess.kill();
   }
 })();
