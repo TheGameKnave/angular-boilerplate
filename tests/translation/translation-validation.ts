edbe3347895c25b41f8692e6776f24154ac1f57c
Ralph L Robinson <ralphrobinson@student.olympic.edu>
2024-08-14 09:25:05 -0700

    Fixed pathes in translation-validation for both platforms?

--- tests/translation/translation-validation.ts
+++ tests/translation/translation-validation.ts
@@ -1,11 +1,14 @@
 import { exec } from 'child_process';
 import { SUPPORTED_LANGUAGES } from '..\\..\\client\\src\\app\\helpers\\constants';
+import * as path from 'path';
 
-const schemaPath = 'translation.schema.json';
-const translationsDir = '..\\..\\client\\src\\assets\\i18n';
+const schemaPath = path.join(__dirname, 'translation.schema.json');
+const translationsDir = path.join(__dirname, '..', '..', 'client', 'src', 'assets', 'i18n');
+const ajvPath = path.join(__dirname, '..', '..', 'node_modules', '.bin', 'ajv');
+
 
 SUPPORTED_LANGUAGES.forEach((lang) => {
-  const command = `../../node_modules/.bin/ajv -s ${schemaPath} -d ${translationsDir}/${lang}.json --strict=false`;
+  const command = `${ajvPath} -s ${schemaPath} -d ${translationsDir}/${lang}.json --strict=false`;
 
   exec(command, (error, stdout, stderr) => {
     if (error) {
